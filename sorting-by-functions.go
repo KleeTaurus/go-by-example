93ec8f4b075e19c67f6d23ef49bfd9ec911258de
Kevin Lee <klee.taurus@gmail.com>
2020-02-09 10:44:01 +0800

    add comments for sorting examples

--- sorting-by-functions.go
+++ sorting-by-functions.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
+/*
+Sometimes we’ll want to sort a collection by something other than its natural
+order. For example, suppose we wanted to sort strings by their length instead
+of alphabetically. Here’s an example of custom sorts in Go.
+*/
+
+// In order to sort by a custom function in Go, we need a corresponding type.
+// Here we’ve created a byLength type that is just an alias for the builtin
+// []string type.
 type byLength []string
 
+/*
+We implement sort.Interface - Len, Less, and Swap - on our type so we can use
+the sort package’s generic Sort function. Len and Swap will usually be similar
+across types and Less will hold the actual custom sorting logic. In our case
+we want to sort in order of increasing string length, so we use len(s[i]) and
+len(s[j]) here.
+*/
 func (s byLength) Len() int {
 	return len(s)
 }
@@ -19,6 +37,9 @@ func (s byLength) Less(i, j int) bool {
 
 func main() {
 	fruits := []string{"peach", "banana", "kiwi"}
+	// With all of this in place, we can now implement our custom sort by
+	// converting the original fruits slice to byLength, and then use sort.Sort
+	// on that typed slice.
 	sort.Sort(byLength(fruits))
 	fmt.Println(fruits)
 }
